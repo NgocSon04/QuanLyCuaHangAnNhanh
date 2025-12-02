f74f0c12302bce5eb2eb0ed11b669e8df887bb1d
NgocSon04 <nguyenngocson7704@gmail.com>
2025-12-03 00:37:20 +0700

    mainview

---
+++ src/CH/model/DatMon.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CH.model;
+
+/**
+ *
+ * @author Ngoc Son
+ */
+public class DatMon {
+    
+}
